f82b876298a56c38f665503264710561dd1d85da
Skye N <tylern662@gmail.com>
2025-07-29 06:02:18 -0400

    final

---
+++ app/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BumblesSimulator from './app';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BumblesSimulator />
+  </React.StrictMode>,
+);
